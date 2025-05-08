f1c91d13000a4e23f5e287e40d4b05908bf927a7
unknown <colinstimsontools@stimson.tech>
2025-05-07 20:35:38 -0500

    added styling to overhaul navbar for react support, also added tags to specify that AI bots such as openai or meta are not allowed to crawl this page.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';//css file linked.
 
 export default function Navbar() {
     return (
-        <nav id="mainNavbar">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/articles">Articles</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/dashboard">Dashboard</Link></li>
-                <li><Link to="/create">Create</Link></li>
-            </ul>
-        </nav>
+        <div>
+            <div className='navbarLogo'><img src='/logo192.png'></img></div>
+            <nav id="mainNavbar">
+
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/articles">Articles</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/dashboard">Dashboard</Link></li>
+                    <li><Link to="/create">Create</Link></li>
+                </ul>
+            </nav>
+        </div>
     );
 }
